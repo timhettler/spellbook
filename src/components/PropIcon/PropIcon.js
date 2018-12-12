4acca95cc7b9bde51c184a4f5fad926150968579
Tim Hettler <me@timhettler.com>
2018-12-12 13:24:34 -0500

    add titles to icons

--- src/components/PropIcon/PropIcon.js
+++ src/components/PropIcon/PropIcon.js
@@ -4,49 +4,35 @@ import uuidv4 from '../../utilities/uuidv4';
 
 import './PropIcon.scss';
 
-export default function PropIcon({ type }) {
-  let id;
+const icon = {
+  ritual: '🕯',
+  concentration: '🌀',
+  cost: '💰',
+};
 
-  switch (type) {
-    case 'ritual':
-      id = uuidv4();
-      return (
-        <div className="prop-icon">
-          <span role="img" aria-labelledby={id}>
-            🕯
-          </span>
-          <span id={id} className="visuallyHidden">
-            ritual
-          </span>
-        </div>
-      );
-    case 'concentration':
-      id = uuidv4();
-      return (
-        <div className="prop-icon">
-          <span role="img" aria-labelledby={id}>
-            🌀
-          </span>
-          <span id={id} className="visuallyHidden">
-            concentration
-          </span>
-        </div>
-      );
-    case 'cost':
-      id = uuidv4();
-      return (
-        <div className="prop-icon">
-          <span role="img" aria-labelledby={id}>
-            💰
-          </span>
-          <span id={id} className="visuallyHidden">
-            has material cost
-          </span>
-        </div>
-      );
-    default:
-      return null;
+const label = {
+  ritual: 'Can be cast as a ritual',
+  concentration: 'Requires concentration',
+  cost: 'Has a material cost',
+};
+
+export default function PropIcon({ type }) {
+  const id = uuidv4();
+
+  if (!type) {
+    return null;
   }
+
+  return (
+    <div className="prop-icon">
+      <span role="img" title={label[type]} aria-hidden={true}>
+        {icon[type]}
+      </span>
+      <span id={id} className="visuallyHidden">
+        {label[type]}
+      </span>
+    </div>
+  );
 }
 
 PropIcon.propTypes = {
