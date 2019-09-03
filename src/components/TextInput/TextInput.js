5d944b4e8a7fdc47957456725dc7a9882019b54b
Tim Hettler <hettler@work.co>
2019-09-03 15:49:07 -0400

    add datalist support for TextInput

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+
+import uuidv4 from '../../utilities/uuidv4';
 
 import VisuallyHidden from '../VisuallyHidden';
 import './TextInput.scss';
@@ -11,19 +13,35 @@ function handleChange(callback) {
   };
 }
 
-const TextInput = ({ value, className, onChange, label, ...rest }) => (
-  <label className={classNames('text-label')}>
-    <VisuallyHidden>{label}</VisuallyHidden>
-    <input
-      className={classNames('text-input', className)}
-      type="text"
-      value={value}
-      onChange={handleChange(onChange)}
-      autoComplete="off"
-      {...rest}
-    />
-  </label>
-);
+const TextInput = ({ value, className, onChange, label, list, ...rest }) => {
+  const [datalistId] = useState(uuidv4());
+
+  return (
+    <label className={classNames('text-label')}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <input
+        className={classNames('text-input', className)}
+        type="text"
+        value={value}
+        onChange={handleChange(onChange)}
+        autoComplete="off"
+        list={list.length && datalistId}
+        {...rest}
+      />
+      {list.length && (
+        <datalist id={datalistId}>
+          {list.map(item => (
+            <option key={item} value={item} />
+          ))}
+        </datalist>
+      )}
+    </label>
+  );
+};
+
+TextInput.defaultProps = {
+  list: [],
+};
 
 TextInput.propTypes = {
   value: PropTypes.string.isRequired,
@@ -33,6 +51,7 @@ TextInput.propTypes = {
   ]),
   onChange: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
+  list: PropTypes.array,
 };
 
 export default TextInput;
