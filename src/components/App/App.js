46d66d245b1ab76ed93d248a4cf03cbe9e77ef10
Tim Hettler <hettler@work.co>
2019-11-19 12:57:38 -0500

    move all business logic to container, refactor with hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,100 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-
-import {
-  loadSpells,
-  loadClasses,
-  loadSchools,
-  addSubclass,
-  viewSpell,
-} from '../../actions';
-import * as data from '../../data';
-import VisibleSpellList from '../../containers/VisibleSpellList';
-import SelectedSpellDetail from '../../containers/SelectedSpellDetail';
-import VisibleControls from '../../containers/VisibleControls';
-import OfflineToast from '../../containers/OfflineToast';
-import history from '../../utilities/history';
 
 import './App.scss';
 
-function _renderNoSpellSelected() {
+const App = ({ currentSpellId, controls, list, detail, toast }) => {
+  const [isSpellSelected, setIsSpellSelected] = useState(false);
+
+  useEffect(() => {
+    setIsSpellSelected(!!currentSpellId);
+  }, [currentSpellId]);
+
   return (
-    <div className="no-spell-selected">
-      <h2 className="no-spell-selected__header">No Spell Selected</h2>
-      <p className="no-spell-selected__copy">
-        Select a spell from the list to see its details
-      </p>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Spellbook</h1>
+      </header>
+      <main className="App-main">
+        <div className="App-main-container">
+          <section className="App-section App-section--list">
+            <nav className="App-spell-list-nav">{controls}</nav>
+            <div className="App-spell-list">
+              <div className="App-spell-list-container">{list}</div>
+            </div>
+          </section>
+          <section
+            className={classNames('App-section App-section--detail', {
+              'is-active': isSpellSelected,
+            })}
+          >
+            {detail}
+          </section>
+        </div>
+      </main>
+      <aside className="App-toast">{toast}</aside>
     </div>
   );
-}
+};
 
-class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(loadSpells(data.SPELLS));
-    this.props.dispatch(loadClasses(data.CLASSES));
-    this.props.dispatch(loadSchools(data.SCHOOLS));
-
-    this.props.dispatch(addSubclass('Ranger', 'archetypes', data.ARCHETYPES));
-    this.props.dispatch(addSubclass('Druid', 'circles', data.CIRCLES));
-    this.props.dispatch(addSubclass('Cleric', 'domains', data.DOMAINS));
-    this.props.dispatch(addSubclass('Paladin', 'oaths', data.OATHS));
-    this.props.dispatch(addSubclass('Warlock', 'patrons', data.PATRONS));
-
-    const loc = history.location.pathname.split('/').filter(x => x);
-    if (loc[0] === 'spell') {
-      this.props.dispatch(viewSpell(loc[1]));
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    this.updateHistory();
-  }
-
-  updateHistory() {
-    if (!this.props.currentSpellId) {
-      history.push('');
-      return;
-    }
-
-    history.push(`/spell/${this.props.currentSpellId}`);
-  }
-
-  render() {
-    let isSpellDetailActive = !!this.props.currentSpellId;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Spellbook</h1>
-        </header>
-        <main className="App-main">
-          <div className="App-main-container">
-            <section className="App-section App-section--list">
-              <nav className="App-spell-list-nav">
-                <VisibleControls />
-              </nav>
-              <div className="App-spell-list">
-                <div className="App-spell-list-container">
-                  <VisibleSpellList />
-                </div>
-              </div>
-            </section>
-            <section
-              className={classNames('App-section App-section--detail', {
-                'is-active': isSpellDetailActive,
-              })}
-            >
-              {isSpellDetailActive && <SelectedSpellDetail />}
-              {!isSpellDetailActive && _renderNoSpellSelected()}
-            </section>
-          </div>
-        </main>
-        <aside className={classNames('App-toast')}>
-          <OfflineToast />
-        </aside>
-      </div>
-    );
-  }
-}
+App.propTypes = {
+  currentSpellId: PropTypes.string,
+  controls: PropTypes.element,
+  list: PropTypes.element,
+  detail: PropTypes.element,
+  toast: PropTypes.element,
+};
 
 export default App;
--- src/containers/ConnectedApp/ConnectedApp.js
+++ src/containers/ConnectedApp/ConnectedApp.js
@@ -1,10 +1,78 @@
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  loadSpells,
+  loadClasses,
+  loadSchools,
+  addSubclass,
+  viewSpell,
+} from '../../actions';
+import * as data from '../../data';
+import history from '../../utilities/history';
 import App from '../../components/App';
 
-function mapStateToProps(state) {
-  return {
-    currentSpellId: state.currentSpellId,
-  };
-}
+import VisibleSpellList from '../../containers/VisibleSpellList';
+import SelectedSpellDetail from '../../containers/SelectedSpellDetail';
+import VisibleControls from '../../containers/VisibleControls';
+import OfflineToast from '../../containers/OfflineToast';
 
-export default connect(mapStateToProps)(App);
+const renderNoSpellSelected = () => (
+  <div className="no-spell-selected">
+    <h2 className="no-spell-selected__header">No Spell Selected</h2>
+    <p className="no-spell-selected__copy">
+      Select a spell from the list to see its details
+    </p>
+  </div>
+);
+
+const renderDetail = currentSpellId =>
+  !!currentSpellId ? <SelectedSpellDetail /> : renderNoSpellSelected();
+
+const ConnectedApp = () => {
+  const currentSpellId = useSelector(state => state.currentSpellId);
+  const dispatch = useDispatch();
+
+  // Load in data sources
+  useEffect(() => {
+    dispatch(loadSpells(data.SPELLS));
+    dispatch(loadClasses(data.CLASSES));
+    dispatch(loadSchools(data.SCHOOLS));
+
+    dispatch(addSubclass('Ranger', 'archetypes', data.ARCHETYPES));
+    dispatch(addSubclass('Druid', 'circles', data.CIRCLES));
+    dispatch(addSubclass('Cleric', 'domains', data.DOMAINS));
+    dispatch(addSubclass('Paladin', 'oaths', data.OATHS));
+    dispatch(addSubclass('Warlock', 'patrons', data.PATRONS));
+  }, [dispatch]);
+
+  // If url includes a spell id, load that spell
+  useEffect(() => {
+    const loc = history.location.pathname.split('/').filter(x => x);
+    if (loc[0] === 'spell') {
+      dispatch(viewSpell(loc[1]));
+    }
+  }, [dispatch]);
+
+  // When spell id changes, update the URL
+  useEffect(() => {
+    if (!currentSpellId) {
+      history.push('');
+      return;
+    }
+
+    history.push(`/spell/${currentSpellId}`);
+  }, [currentSpellId]);
+
+  return (
+    <App
+      currentSpellId={currentSpellId}
+      controls={<VisibleControls />}
+      list={<VisibleSpellList />}
+      detail={renderDetail(currentSpellId)}
+      toast={<OfflineToast />}
+    />
+  );
+};
+
+export default ConnectedApp;
