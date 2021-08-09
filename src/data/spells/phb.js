be49f1bff4fac045cba6168289bf61a10b03433a
Tim Hettler <hettler@work.co>
2021-08-09 15:41:08 -0400

    fix: scrying description

--- src/data/spells/phb.js
+++ src/data/spells/phb.js
@@ -4635,7 +4635,25 @@ You can store a prepared spell of 3rd level or lower in the glyph by casting it 
     },
     {
       name: 'Scrying',
-      desc: "<p>You can see and hear a particular creature you choose that is on the same plane of existence as you. The target must make a Wisdom saving throw, which is modified by how well you know the target and the sort of physical connection you have to it. If a target knows you're casting this spell, it can fail the saving throw voluntarily if it wants to be observed.</p><h3>Knowledge &amp; Save Modifie</h3><p></p><p>Secondhand (you have heard of the target) +5</p><p>Firsthand (you have met the target) +0</p><p>Familiar (you know the target well) -5</p><h3>Connection &amp; Save Modifie</h3><p></p><p>Likeness or picture -2</p><p>Possession or garment -4</p><p>Body part, lock of hair, bit of nail, or the like -10</p><p>On a successful save, the target isn't affected, and you can't use this spell against it again for 24 hours.</p><p>On a failed save, the spell creates an invisible sensor within 10 feet of the target. You can see and hear through the sensor as if you were there. The sensor moves with the target, remaining within 10 feet of it for the duration. A creature that can see invisible objects sees the sensor as a luminous orb about the size of your fist.</p><p>Instead of targeting a creature, you can choose a location you have seen before as the target of this spell. When you do, the sensor appears at that location and doesn't move.</p>",
+      desc: `You can see and hear a particular creature you choose that is on the same plane of existence as you. The target must make a Wisdom saving throw, which is modified by how well you know the target and the sort of physical connection you have to it. If a target knows you're casting this spell, it can fail the saving throw voluntarily if it wants to be observed.
+
+| Knowledge | Save Modifier |
+| --- | --- |
+| Secondhand (you have heard of the target) | +5 |
+| Firsthand (you have met the target) | 0 |
+| Familiar (you know the target well) | -5 |
+
+| Connection | Save Modifier |
+| --- | --- |
+| Likeness or picture | -2 |
+| Possession or garment | -4 |
+| Body part, lock of hair, bit of nail, or the like | -10 |
+
+On a successful save, the target isn't affected, and you can't use this spell against it again for 24 hours.
+
+On a failed save, the spell creates an invisible sensor within 10 feet of the target. You can see and hear through the sensor as if you were there. The sensor moves with the target, remaining within 10 feet of it for the duration. A creature that can see invisible objects sees the sensor as a luminous orb about the size of your fist.
+
+Instead of targeting a creature, you can choose a location you have seen before as the target of this spell. When you do, the sensor appears at that location and doesn't move.`,
       page: 'phb 273',
       range: 'Self',
       components: ['V', 'S', 'M'],
